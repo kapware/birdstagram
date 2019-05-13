4dca47de40aa661c9b35d59a95459003fff8a7ba
Pawel Kapala <github@kapware.com>
2019-05-13 16:06:31 +0200

    Husky lint support

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports =  {
+    parser:  '@typescript-eslint/parser',
+    extends:  [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions:  {
+        ecmaVersion:  2018,
+        sourceType:  'module',
+        ecmaFeatures:  {
+            jsx:  true,
+        },
+    },
+    plugins: [
+        "react",
+        "react-native"
+    ],
+    rules:  {
+        "@typescript-eslint/explicit-member-accessibility": "off",
+        "@typescript-eslint/explicit-function-return-type": "off"
+    },
+    settings:  {
+        react:  {
+            version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+        },
+    },
+};
